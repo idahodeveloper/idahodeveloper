ba99c115de07afff451bd866253b08c2b8747c77
idaho developer <idahodeveloper@protonmail.com>
2019-01-11 18:52:28 -0500

    update everything and touch if necessary

--- mine.sh
+++ mine.sh
@@ -1,15 +1,15 @@
-!#/bin/sh
+#!/bin/sh
 cd /home/idahodeveloper/src/idahodeveloper/;
 echo `date +%Y%m%d%H%M%S`;
 time echo `date +%Y%m%d%H%M%S` > touchme;
-time cat composer.json;
-time composer outdated;
-time composer outdated drupal/*;
+time cat composer.json > run;
+time composer outdated >> run;
+time composer outdated drupal/* >> run;
 time rm composer.lock;
-time composer update drupal/* composer/installers webflo/drupal-core-strict --with-dependencies;
-time git status;
+time composer update drupal/* composer/installers webflo/drupal-core-strict --with-dependencies >> run;
+time git status >> run;
 time git add .;
 time git commit -m "update everything and touch if necessary";
-time git pull;
-time git push;
+time git pull origin master --strategy-option=theirs;
+time git push origin master;
 
